4ec4d8408dc985d6cfcaf0e7e0042ccd2c91a83e
daniherreraa <herreraacevedodl@gmail.com>
2024-11-08 23:36:27 -0500

    Agregar componente UserNavbar y actualizar dependencias; incluir .env en .gitignore

--- frontend/app/playground/page.jsx
+++ frontend/app/playground/page.jsx
@@ -1,21 +1,12 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { InboxIcon } from "lucide-react";
-import ModelGenerator from "@/components/ui/modelgenerator";
 
+import ModelGenerator from "@/components/ui/modelgenerator";
+import UserNavbar from "@/components/ui/userNavbar";
 export default function Page() {
   return (
     <div className="w-svw h-svh flex flex-col py-3 px-4 bg-background-material dark:bg-background-material-dark overflow-x-hidden">
       <main className="relative">
         {/*Mobile - File uploader*/}
-        <div className="flex flex-row items-center justify-between w-full h-[5svh]">
-          <div>
-            <InboxIcon className="text dark:text-on-surface-dark" />
-          </div>
-          <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>User</AvatarFallback>
-          </Avatar>
-        </div>
+        <UserNavbar />
         <div>
           <ModelGenerator />
         </div>
---
+++ frontend/components/ui/userNavbar.jsx
@@ -0,0 +1,21 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { InboxIcon } from "lucide-react";
+import { auth } from "@/auth";
+
+const UserNavbar = async () => {
+  const session = await auth();
+
+  return (
+    <div className="flex flex-row items-center justify-between w-full h-[5svh]">
+      <div>
+        <InboxIcon className="text dark:text-on-surface-dark" />
+      </div>
+      <Avatar>
+        <AvatarImage src={session.user.image} />
+        <AvatarFallback>User</AvatarFallback>
+      </Avatar>
+    </div>
+  );
+};
+
+export default UserNavbar;
